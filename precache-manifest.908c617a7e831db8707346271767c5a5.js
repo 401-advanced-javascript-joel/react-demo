2718e4a535a36226d192665d3c910ed59214fc53
Joel Watson <joel.watson92@gmail.com>
2020-06-10 20:40:45 -0700

    Updates

---
+++ precache-manifest.908c617a7e831db8707346271767c5a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3366ca2bf6626a5dc29d6256c4a3acdd",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "f7ad747ca34d70eb84dc",
+    "url": "/react-demo/static/css/main.92ba19c8.chunk.css"
+  },
+  {
+    "revision": "baae20de16e6286fc564",
+    "url": "/react-demo/static/js/2.31b9ed96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo/static/js/2.31b9ed96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ad747ca34d70eb84dc",
+    "url": "/react-demo/static/js/main.43de4486.chunk.js"
+  },
+  {
+    "revision": "77d261d29d994ee1b384",
+    "url": "/react-demo/static/js/runtime-main.01055763.js"
+  },
+  {
+    "revision": "39d3e83dd2ebfa0a36c17c11a0389295",
+    "url": "/react-demo/static/media/pic1.39d3e83d.jpeg"
+  }
+]);
